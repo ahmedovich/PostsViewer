e27ddbf91711782721cf06eb5b0a668c8ec666b9
Ahmedovich <Codekraking@gmail.com>
2021-08-24 20:54:37 +0300

    Ok!

--- src/features/login/components/authentication.styles.tsx
+++ src/features/login/components/authentication.styles.tsx
@@ -2,12 +2,113 @@ import styled from 'styled-components/native';
 import {Card} from 'react-native-paper';
 
 export const InputBox = styled.TextInput`
-  width: 300;
-  background-color: '#a9aaad';
-  color: '#434c5e';
-  padding-horizontal: 16;
-  border-radius: 20;
-  elevation: 5;
-  margin-vertical: 10;
-  fontsize: 16;
+<<<<<<< HEAD
+<<<<<<< HEAD
+  width: 300px;
+  background-color: #a9aaad;
+  color: #434c5e;
+  padding-horizontal: 16px;
+  border-radius: 20px;
+  margin-vertical: 10px;
+  font-size: 16px;
+`;
+
+export const SaveLogin = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-vertical: 20px;
+`;
+
+export const CheckBoxView = styled.View`
+  flex-direction: row;
+  align-items: center;
+  flex: 0.7;
+  justify-content: flex-start;
+  padding-left: 15px;
+`;
+
+/*
+export const CheckBox = styled.CheckBox`
+  width: 10px;
+  height: 10px;
+  margin-left: 10px;
+`;
+*/
+
+export const CheckText = styled.Text`
+  color: #c9c9ca;
+  padding-left: 25px;
+  font-size: 15px;
+  font-weight: 500;
+`;
+
+export const CheckTextView = styled.View`
+  flex: 0.5;
+  justify-content: flex-end;
+`;
+
+export const ForgetText = styled.Text`
+  color: #ff004c;
+  font-size: 15px;
+  font-weight: 500;
+`;
+
+export const ButtonContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const SignUpButtonContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 16px;
+  font-weight: 500;
+  color: #c9c9ca;
+  align-self: center;
+  padding: 10px;
+`;
+
+export const ButtonTextRed = styled.Text`
+  font-size: 16px;
+  font-weight: 500;
+  color: #ff004c;
+  align-self: center;
+  padding: 10px;
+`;
+
+export const ButtonGray = styled.TouchableOpacity`
+  width: 50%;
+  height: 40px;
+  background-color: #2e3440;
+  border-radius: 20px;
+  font-size: 16px;
+  margin: 15px;
+`;
+
+export const ButtonTransparent = styled.TouchableOpacity`
+  width: 100%;
+  height: 40px;
+  border-radius: 20px;
+  font-size: 16px;
+  margin: 15px;
+`;
+
+export const SignUpButton = styled.TouchableOpacity`
+  width: 80%;
+  height: 40px;
+  background-color: #2e3440;
+  border-radius: 20px;
+  font-size: 16px;
+  margin: 15px;
+`;
+
+export const SignUpQuestionText = styled.Text`
+  justify-content: flex-start;
+  color: #c9c9ca;
+  font-size: 15px;
+  font-weight: 500;
 `;
